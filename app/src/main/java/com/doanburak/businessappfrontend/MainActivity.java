57533db6808045ca0d6f8fb50d32a286bf758241
doanb <doanburak97@gmail.com>
2021-06-01 18:37:32 +0300

    commit 7

--- app/src/main/java/com/doanburak/businessappfrontend/MainActivity.java
+++ app/src/main/java/com/doanburak/businessappfrontend/MainActivity.java
@@ -11,9 +11,19 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,6 +60,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 tv_startAndEndDate.setText("Selected dates : " + "\n" + startDate + " " + endDate);
                 tv_prediction.setText("Buraya tahmin edilmiş veri gelecek.");
+
+                String url = "";
+                StringRequest sr = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(MainActivity.this, response.trim(), Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        }){
+                    protected Map<String, String> getParams(){
+                        Map<String, String> params = new HashMap<>();
+                        params.put("startDate",startDate);
+                        params.put("endDate",endDate);
+
+                        return params;
+                    }
+                };
+                RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+                requestQueue.add(sr);
             }
         });
     }
